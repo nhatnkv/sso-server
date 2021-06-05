3d2c5ec8a817e01c3d132d0a03207584ef32dbb0
Sotatek-NhatNguyen <nhat.nguyen@sotatek.com>
2021-06-05 12:55:04 +0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+application = Doorkeeper::Application.create(name: "Web client",
+                                             redirect_uri: "http://localhost:3001/users/auth/doorkeeper/callback",
+                                             scopes: ["read", "write"])
+
+Doorkeeper::AccessToken.all.each do |token|
+  token.update(application_id: application.id)
+end
 
 30.times do
   Task.create(name: Faker::Name.name)
